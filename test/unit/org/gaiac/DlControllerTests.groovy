cf3fbe2d35826706b1e51b852683d3e8a775b0d7
Aurélien Maury <amaury@xebia.fr>
2011-10-21 19:17:01 +0200

    added DlController

---
+++ test/unit/org/gaiac/DlControllerTests.groovy
@@ -0,0 +1,17 @@
+package gaiac
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DlController)
+class DlControllerTests {
+
+    void testSomething() {
+
+    }
+}
